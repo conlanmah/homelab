aaec05d0836ca96c13a61765f47e1c08126a73d9
conlanmah <50381040+conlanmah@users.noreply.github.com>
2025-07-22 22:19:47 -0400

    rpi wiregaurd configuration

--- nix-flakes/services/hosts/rpi3-wireguard/configuration.nix
+++ nix-flakes/services/hosts/rpi3-wireguard/configuration.nix
@@ -57,6 +57,51 @@
     };
   };
 
+
+  # enable NAT
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "enu1u1u1";
+  networking.nat.internalInterfaces = [ "wg0" ];
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 ];
+  };
+
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = [ "10.100.5.1/24" ];
+
+      # The port that WireGuard listens to. Must be accessible by the client.
+      listenPort = 51820;
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.5.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # This undoes the above command
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.5.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # Path to the private key file.
+      privateKeyFile = "~/wg-keys/private";
+
+      peers = [
+        # List of allowed peers.
+        { # ThinkpadT14
+          # Feel free to give a meaningful name
+          # Public key of the peer (not a file path).
+          publicKey = "bZcPUveBU+fbNa3dFG93eO6peKhvoMkdOjS3NwelyVo=";
+          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+          allowedIPs = [ "10.100.5.2/32" ];
+        }
+      ];
+    };
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
