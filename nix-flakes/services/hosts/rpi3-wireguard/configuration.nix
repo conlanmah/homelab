226b8fc6bdc90f24ccbd76d9b1ce5cb5a36d7aca
conlanmah <50381040+conlanmah@users.noreply.github.com>
2025-07-24 23:23:41 -0400

    Removed rp filter commands because this is not my laptop conf!

--- nix-flakes/services/hosts/rpi3-wireguard/configuration.nix
+++ nix-flakes/services/hosts/rpi3-wireguard/configuration.nix
@@ -74,15 +74,7 @@
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
 
-    # These commands are nessesary for wireguard
-    extraCommands = ''
-     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
-     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
-    '';
-    extraStopCommands = ''
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
-      ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
-    '';
+
   };
 
   networking.wireguard.interfaces = {
