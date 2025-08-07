8e6fcdbaedbc14e10c7015b0f8ffe1cbf70b6ff5
conlanmah <50381040+conlanmah@users.noreply.github.com>
2025-08-06 20:45:47 -0400

    immich-mars: reenable nftables

--- nix-flakes/services/hosts/immich-mars/configuration.nix
+++ nix-flakes/services/hosts/immich-mars/configuration.nix
@@ -26,6 +26,7 @@
     };
     nameservers = ["192.168.50.1" "8.8.8.8"];
 
+    nftables.enable = true;
     firewall = {
       trustedInterfaces = [ "lo" ]; # nessesary because it's a container
       enable = true;
