081a45e151bf7acc7b4ba274aae78ac9f38857ed
conlanmah <50381040+conlanmah@users.noreply.github.com>
2025-08-07 14:49:29 -0400

    immich-mars: reset host binding to all interfaces

--- nix-flakes/services/hosts/immich-mars/configuration.nix
+++ nix-flakes/services/hosts/immich-mars/configuration.nix
@@ -59,7 +59,7 @@
   services.immich = {
     enable = true;
     port = 2283;
-    host = "192.168.100.102";
+    host = "0.0.0.0";
     openFirewall = true;
     mediaLocation = "/mnt/immich-mars";
   };
