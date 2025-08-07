568f98eade9773f8f7ee8b75254c01c0f7e8699a
conlanmah <50381040+conlanmah@users.noreply.github.com>
2025-08-06 20:25:07 -0400

    gruvbox: limit firewall port ALLOWs to only the storage VLAN

--- nix-flakes/services/hosts/gruvbox/configuration.nix
+++ nix-flakes/services/hosts/gruvbox/configuration.nix
@@ -26,6 +26,9 @@
       eno2.ipv4.addresses = [{
         address = "192.168.150.4";
         prefixLength = 24;
+        # for NFSv3; view with `rpcinfo -p`
+        allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
+        allowedUDPPorts = [ 111 2049 4000 4001  4002 20048 ];
       }];
     };
 
@@ -38,9 +41,7 @@
 
     firewall = {
       enable = true;
-      # for NFSv3; view with `rpcinfo -p`
-      allowedTCPPorts = [ 111  2049 4000 4001 4002 20048 ];
-      allowedUDPPorts = [ 111 2049 4000 4001  4002 20048 ];
+
     };
   };
 
