357002c9787a132ccaf189a3e259dca1ea4c5154
conlanmah <50381040+conlanmah@users.noreply.github.com>
2026-05-28 19:56:56 -0400

    Fix nix-ct-test-02 container config

--- nix-flakes/services/hosts/nix-ct-test-02/configuration.nix
+++ nix-flakes/services/hosts/nix-ct-test-02/configuration.nix
@@ -2,60 +2,68 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
 
-                    ############################################################
-                    # Container / Proxmox LXC baseline
-                    ############################################################
-                    boot.isContainer = true;
+  ############################################################
+  # Container / Proxmox LXC baseline
+  ############################################################
+  boot.isContainer = true;
 
-                    # Allows Terraform to set password 
-                    users.mutableUsers = true;
+  # Allows Terraform to set password
+  users.mutableUsers = true;
+  nixpkgs.hostPlatform = "x86_64-linux";
+  imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
 
-                    # Proxmox typically governs filtering; avoid "SSH is up but blocked"
-                    # networking.firewall.enable = false;
+  # Proxmox typically governs filtering; avoid "SSH is up but blocked"
+  # networking.firewall.enable = false;
 
-                    # Helpful in containers: don't block boot waiting for "online"
-                    systemd.network.wait-online.enable = false;
-                    systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+  # Helpful in containers: don't block boot waiting for "online"
+  systemd.network.wait-online.enable = false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
-                    ############################################################
-                    # Networking (Proxmox LXC generally presents eth0)
-                    ############################################################
-                    networking.hostName = "nixos-lxc";
+  ############################################################
+  # Networking (Proxmox LXC generally presents eth0)
+  ############################################################
+  networking.hostName = "nixos-lxc";
 
-                    # Be explicit: use systemd-networkd and DHCP on eth0.
-                    # networking.useNetworkd = true; # Experimental
-                    systemd.network.enable = true;
-                    networking.useDHCP = false;
-                    # networking.interfaces.eth0.useDHCP = false;
+  # Be explicit: use systemd-networkd and DHCP on eth0.
+  # networking.useNetworkd = true; # Experimental
+  systemd.network.enable = true;
+  networking.useDHCP = false;
+  # networking.interfaces.eth0.useDHCP = false;
 
-                    services.resolved.enable = true;
+  services.resolved.enable = true;
 
-                    ############################################################
-                    # SSH 
-                    ############################################################
-                    services.openssh = {
-                      enable = true;
-                      openFirewall = true;
-                      settings = {
-                        PasswordAuthentication = false;
-                        KbdInteractiveAuthentication = false;
-                        PermitRootLogin = "prohibit-password";
-                      };
-                    };
-                   
-                    security.sudo = {
-                      enable = true;
-                      wheelNeedsPassword = false;
-                    };
+  ############################################################
+  # SSH
+  ############################################################
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
 
-                    ############################################################
-                    # Container niceties / reduce console noise
-                    ############################################################
-                    # Proxmox console works, but getty management can be weird in LXCs.
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
+
+  ############################################################
+  # Container niceties / reduce console noise
+  ############################################################
+  # Proxmox console works, but getty management can be weird in LXCs.
+  ###### NEVER CHANGE THIS
+  system.stateVersion = "25.05";
 }
-
-
